6a75ba8c4d3fbf5184256a45704543c76d26a2e2
Wesley Cota <wlcota@gmail.com>
2018-10-14 16:22:26 -0300

    Create example.f90

---
+++ example.f90
@@ -0,0 +1,18 @@
+program example_tictoc
+use mod_tictoc
+implicit none
+
+    type(tictoc)    :: ctimer
+    integer         :: i, j
+    real*8          :: x(10000)
+    
+    call ctimer%start()
+    call ctimer%tic()
+    do j=1,100000
+        x(:) = log(23.)*[ (i,i=1,10000) ]
+    enddo
+    call ctimer%toc()
+    
+    write(*,'(a,f)') 'CPU TIME = ', ctimer%t_tot
+
+end program
