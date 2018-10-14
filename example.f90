9b25bea998fb15984918a9f33cf9e12f9ddecc18
Wesley Cota <wlcota@gmail.com>
2018-10-14 16:24:07 -0300

    Update example.f90

--- example.f90
+++ example.f90
@@ -14,5 +14,16 @@ implicit none
     call ctimer%toc()
     
     write(*,'(a,f)') 'CPU TIME = ', ctimer%t_tot
+    
+    write(*,'(a)') 'Now measuring inside the loop...'
+    
+    call ctimer%reset()
+    do j=1,100000
+        call ctimer%tic()
+        x(:) = log(23.)*[ (i,i=1,10000) ]
+        call ctimer%toc()
+    enddo
+    
+    write(*,'(a,f)') 'CPU TIME = ', ctimer%t_tot
 
 end program
