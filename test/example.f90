9c262616671a1d4caf911aead8f7507dafe50ef8
Wesley Cota <wlcota@gmail.com>
2024-02-22 20:51:44 -0300

    adapt example

--- test/example.f90
+++ test/example.f90
@@ -1,29 +1,31 @@
 program example_tictoc
-use mod_tictoc
-implicit none
+   use mod_tictoc
+   implicit none
 
-    type(tictoc)    :: ctimer
-    integer         :: i, j
-    real*8          :: x(10000)
-    
-    call ctimer%start()
-    call ctimer%tic()
-    do j=1,100000
-        x(:) = log(23.)*[ (i,i=1,10000) ]
-    enddo
-    call ctimer%toc()
-    
-    write(*,'(a,f)') 'CPU TIME = ', ctimer%t_tot
-    
-    write(*,'(a)') 'Now measuring inside the loop...'
-    
-    call ctimer%reset()
-    do j=1,100000
-        call ctimer%tic()
-        x(:) = log(23.)*[ (i,i=1,10000) ]
-        call ctimer%toc()
-    enddo
-    
-    write(*,'(a,f)') 'CPU TIME = ', ctimer%t_tot
+   integer, parameter :: dp = selected_real_kind(15)
+
+   type(tictoc)    :: ctimer
+   integer         :: i, j
+   real(kind=dp)          :: x(10000)
+
+   call ctimer%start()
+   call ctimer%tic()
+   do j = 1, 100000
+      x(:) = log(23.)*[(i, i=1, 10000)]
+   end do
+   call ctimer%toc()
+
+   write (*, '(a,f)') 'CPU TIME = ', ctimer%t_tot
+
+   write (*, '(a)') 'Now measuring inside the loop...'
+
+   call ctimer%reset()
+   do j = 1, 100000
+      call ctimer%tic()
+      x(:) = log(23.)*[(i, i=1, 10000)]
+      call ctimer%toc()
+   end do
+
+   write (*, '(a,f)') 'CPU TIME = ', ctimer%t_tot
 
 end program
